14d055de41e0da193bbbd99a5cbff2c339518bf3
youngspring1 <youngspring1@163.com>
2016-12-26 13:15:05 +0800

    update txt

--- txt2speech_test.js
+++ txt2speech_test.js
@@ -2,8 +2,8 @@
 var bingClientTTS = new BingTTS.Client("46adbdd87c3e442d92bb33e7f5b5d317");
 document.getElementById("speakBtn").addEventListener("click", function () {
     bingClientTTS.multipleXHR = document.getElementById("multipleXHRChk").checked;
-    bingClientTTS.synthesize("how are you", BingTTS.SupportedLocales.jpJP_Female);
-    bingClientTTS.synthesize("天気がいいから、散歩しましょう。", BingTTS.SupportedLocales.jpJP_Female);
+    bingClientTTS.synthesize("line1:how are you", BingTTS.SupportedLocales.jpJP_Female);
+    bingClientTTS.synthesize("line2:天気がいいから、散歩しましょう。", BingTTS.SupportedLocales.jpJP_Female);
 //    bingClientTTS.synthesize("Salut, je m'appelle David", BingTTS.SupportedLocales.frFR_Female);
 //    bingClientTTS.synthesize("Enchanté de faire votre connaissance!", BingTTS.SupportedLocales.frFR_Male);
 //    bingClientTTS.synthesize("Hallo, mein Name ist Frank.", BingTTS.SupportedLocales.deDE_Male);
